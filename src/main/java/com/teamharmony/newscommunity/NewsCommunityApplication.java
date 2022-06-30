9d6a87e66fde53d7eb3748a90bc2aaa2cbaa79b0
Yeah-PC\blueflow <blueflow109@gmail.com>
2022-06-30 19:49:12 +0900

    Add: add a user domain

--- src/main/java/com/teamharmony/newscommunity/NewsCommunityApplication.java
+++ src/main/java/com/teamharmony/newscommunity/NewsCommunityApplication.java
@@ -1,18 +1,13 @@
 package com.teamharmony.newscommunity;
 
-import com.teamharmony.newscommunity.users.entity.Role;
-import com.teamharmony.newscommunity.users.entity.RoleType;
-import com.teamharmony.newscommunity.users.entity.User;
-import com.teamharmony.newscommunity.users.service.UserService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.ArrayList;
-
+@EnableJpaAuditing // 시간 자동 변경이 가능하도록 합니다.
 @SpringBootApplication
 public class NewsCommunityApplication {
 
@@ -23,16 +18,5 @@ public class NewsCommunityApplication {
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
-	// add a test user
-	@Bean
-	CommandLineRunner run(UserService userService) {
-		return args -> {
-			userService.saveRole(new Role(RoleType.USER));
-			userService.saveRole(new Role(RoleType.ADMIN));
-			userService.saveUser(new User(null,"john","1234","",new ArrayList<>()));
-			userService.addRoleToUser("john",RoleType.USER);
-			userService.addRoleToUser("john",RoleType.ADMIN);
-		};
-	}
+
 }
