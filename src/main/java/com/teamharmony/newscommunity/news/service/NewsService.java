43cf5367b709f70687ae5fc343e1115510ab3756
hyeoKing <cksgurwkd12@naver.com>
2022-07-01 21:23:26 +0900

    FEAT: NewsService에 Repository로부터 뉴스 정보를 받아오는 getNews() 구현

--- src/main/java/com/teamharmony/newscommunity/news/service/NewsService.java
+++ src/main/java/com/teamharmony/newscommunity/news/service/NewsService.java
@@ -11,12 +11,17 @@ import java.util.List;
 @RequiredArgsConstructor
 @Service
 public class NewsService {
+    /* 뉴스 정보 조회 기능을 수행하는 Service*/
     private final NewsRepository newsRepository;
 
     public ResponseNewsDTO getNews(){
+        /* /news의 경로로 요청된 GET 메서드의 응답을 처리하는 메서드, 뉴스정보 리스트를 반환함
+         * param: None
+         * return: ResponseNewsDTO [뉴스 제목, 요약, 이미지_url, 실제뉴스URL, 설명, 작성시간]
+         * */
         List<NewsTable> newsTableList = newsRepository.findAll();
         return ResponseNewsDTO.builder()
-                                .newsTableList(newsTableList)
-                                .build();
+                .newsTableList(newsTableList)
+                .build();
     }
 }
