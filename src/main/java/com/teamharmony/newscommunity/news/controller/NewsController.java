fa5d79e67d4f94237cd43783243c37d4f28f0439
hyeoKing <cksgurwkd12@naver.com>
2022-07-01 21:22:07 +0900

    FEAT: NewsController에 조회를 위한 getNews() 추가

--- src/main/java/com/teamharmony/newscommunity/news/controller/NewsController.java
+++ src/main/java/com/teamharmony/newscommunity/news/controller/NewsController.java
@@ -10,15 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/news")
+@RequestMapping("/api/news")
 @RestController
 public class NewsController {
     private final NewsService newsService;
 
     @GetMapping("")
     public ApiResponse<ResponseNewsDTO> getNews(){
-        /*
-            + index.html에서 뉴스 조회 요청시 DB에 담긴 전체 뉴스 리스트를(ResponseNewsDTO) 리턴
+        /* index.html에서 뉴스 조회 요청시 DB에 담긴 전체 뉴스 리스트를(ResponseNewsDTO) 리턴
             param: None
             return: 뉴스정보(NewsTable)가 담긴 ResponseNewsDTO를 리턴
          */
