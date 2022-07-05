e26a304ffa661af79c1886f996a5f3684c4cd684
YoungeunJo <youngeun.jo1494@gmail.com>
2022-07-05 15:42:54 +0900

    Feat: 고객관리 CRUD 기능 구현

---
+++ src/main/java/com/teamharmony/newscommunity/supports/dto/SupportRequestDto.java
@@ -0,0 +1,11 @@
+package com.teamharmony.newscommunity.supports.dto;
+
+import lombok.Getter;
+
+@Getter
+public class SupportRequestDto {
+    private String username;
+    private String post_title;
+    private String post_content;
+
+}
---
+++ src/main/java/com/teamharmony/newscommunity/supports/repository/SupportRepository.java
@@ -0,0 +1,13 @@
+package com.teamharmony.newscommunity.supports.repository;
+
+
+import com.teamharmony.newscommunity.supports.entity.Support;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SupportRepository extends JpaRepository<Support, Long> {
+    List<Support> findAllByOrderByCreatedAtDesc();
+
+}
+
