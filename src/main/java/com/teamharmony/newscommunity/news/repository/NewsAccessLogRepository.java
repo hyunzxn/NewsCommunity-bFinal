c52c0e66eeb8f8896663a4bfecb87211155a150f
hyeoKing <cksgurwkd12@naver.com>
2022-07-05 14:39:14 +0900

    FIX: AccessLog에 대한 주석 처리로 인해 패키지 찾지 못하는 문제 해결(잉여 import 제거)

---
+++ src/main/java/com/teamharmony/newscommunity/news/repository/NewsAccessLogRepository.java
@@ -0,0 +1,8 @@
+package com.teamharmony.newscommunity.news.repository;
+
+import com.teamharmony.newscommunity.news.entity.NewsAccessLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NewsAccessLogRepository extends JpaRepository<NewsAccessLog, String> {
+
+}
