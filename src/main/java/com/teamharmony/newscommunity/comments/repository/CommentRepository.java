4d36dfc25862edf3344736bf91ce4d18ad05f6ae
hyunzxn <jann1653@gmail.com>
2022-07-06 17:26:24 +0900

    Feat: 댓글 시간순 정렬 기능 구현

--- src/main/java/com/teamharmony/newscommunity/comments/repository/CommentRepository.java
+++ src/main/java/com/teamharmony/newscommunity/comments/repository/CommentRepository.java
@@ -1,10 +1,13 @@
 package com.teamharmony.newscommunity.comments.repository;
 
 import com.teamharmony.newscommunity.comments.entity.Comment;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findAllByNewsId(String newsId);
+    List<Comment> findByNewsIdOrderByCreatedAtDesc(String newsId);
+    List<Comment> findByNewsIdOrderByCreatedAtAsc(String newsId);
 }
--- src/main/java/com/teamharmony/newscommunity/comments/service/CommentService.java
+++ src/main/java/com/teamharmony/newscommunity/comments/service/CommentService.java
@@ -6,6 +6,7 @@ import com.teamharmony.newscommunity.comments.repository.CommentRepository;
 import com.teamharmony.newscommunity.users.entity.User;
 import com.teamharmony.newscommunity.users.repo.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -38,5 +39,13 @@ public class CommentService {
                 () -> new IllegalArgumentException("댓글을 찾을 수 없습니다")
         );
         comment.update(commentRequestDto);
+    }
+
+    public List<Comment> getSortedCommentsDesc(String newsId) {
+        return commentRepository.findByNewsIdOrderByCreatedAtDesc(newsId);
+    }
+
+    public List<Comment> getSortedCommentsAsc(String newsId) {
+        return commentRepository.findByNewsIdOrderByCreatedAtAsc(newsId);
     }
 }
