b120d4a03754ef7d7a4631519bddb37fc1f03c71
Yeah-PC\blueflow <blueflow109@gmail.com>
2022-07-07 21:50:10 +0900

    Feat: 로그인한 사용자 ID를 로컬스토리지에 저장하는 기능 구현

--- src/main/java/com/teamharmony/newscommunity/users/filter/SimpleCorsFilter.java
+++ src/main/java/com/teamharmony/newscommunity/users/filter/SimpleCorsFilter.java
@@ -26,7 +26,7 @@ public class SimpleCorsFilter implements Filter {
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Credentials", "true");
 		
-		response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Authorization, x-csrftoken, content-type");
+		response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Authorization, x-csrftoken, content-type, username");
 		response.setHeader("Access-Control-Expose-Headers", "token");
 		
 		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
