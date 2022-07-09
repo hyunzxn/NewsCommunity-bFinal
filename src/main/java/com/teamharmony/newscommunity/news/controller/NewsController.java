f0674916d32ba2274e14a3f9e389426a3f34b6ff
hyeoKing <cksgurwkd12@naver.com>
2022-07-10 02:50:51 +0900

    DOCS: 뉴스 조회를 위한 컨트롤러 문서 수정

--- src/main/java/com/teamharmony/newscommunity/news/controller/NewsController.java
+++ src/main/java/com/teamharmony/newscommunity/news/controller/NewsController.java
@@ -17,35 +17,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class NewsController {
     private final NewsService newsService;
 
+    // 1. TODO: 뉴스 정보를 일괄 반환하는 /api/news에 대한 GET 요청  //DONE
     @GetMapping("")
     public ApiResponse<ResponseNewsDTO> getNews(){
         /* index.html에서 뉴스 조회 요청시 DB에 담긴 전체 뉴스 리스트를(ResponseNewsDTO) 리턴
             param: None
             return: 뉴스정보(NewsTable)리스트가 담긴 ResponseNewsDTO를 리턴
          */
-//      return ResponseEntity.status(HttpStatus.CREATED).body(newsService.getNews());
+        //return ResponseEntity.status(HttpStatus.CREATED).body(newsService.getNews()); // 학습을 위한 응답 사례 비교 코드
         return ApiResponse.success("result", newsService.getNews());
     }
 
-    @GetMapping("/detail/{newsId}")
-    /*
-        detail.html에서 해당 뉴스의 id 값을 통해 Get요청을 보내는데, 해당 id의 뉴스 정보를 리턴해주는 역할을 한다.
-        param: newsId // 해당 뉴스의 id
-        return: 뉴스정보가 담긴 ResponseNewsDetailDTO
-     */
+    // 2. TODO: 해당 newsId의 상세뉴스정보를 반환하는 /detail/{newsId}에 대한 GET요청  //DONE
+    @GetMapping("/details/{newsId}")
     public ApiResponse<ResponseNewsDetailDTO> getNewsDetial(@PathVariable String newsId){
+        /*  detail.html에서 해당 뉴스의 id 값을 통해 Get요청을 보내는데, 해당 id의 뉴스 정보를 리턴해주는 역할을 한다.
+            param: newsId // 해당 뉴스의 id
+            return: 뉴스정보가 담긴 ResponseNewsDetailDTO
+        */
         return ApiResponse.success("result", newsService.getNewsDetail(newsId));
     }
 
-//    @PostMapping("/log")
-//    public ApiResponse<String> createNewsAccessLog(@RequestBody RequestCreateNewsAccessLogDTO requestCreateNewsAccessLogDTO){
-//        /*
-//            사용자가 뉴스 preview를 클릭해 detail 페이지에 접근할 때마다 기록 작성
-//            param: form('news_id', 'user_id', 'news_title')을 RequestCreateNewsAccessLogDTO에 담아 전달
-//            return: 로그를 작성한 뉴스의 id
-//         */
-//        System.out.println("test");
-//        String newsId = newsService.setNewsAccesLog(requestCreateNewsAccessLogDTO);
-//        return ApiResponse.success("result", newsId);
-//    }
 }
