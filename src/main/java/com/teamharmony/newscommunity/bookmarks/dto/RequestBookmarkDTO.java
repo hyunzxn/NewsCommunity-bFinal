5489015b274a8d1601aa1d242dad30d396df82e9
hyeoKing <cksgurwkd12@naver.com>
2022-07-10 20:37:48 +0900

    FEAT: 북마크 관련 요청, 응답을 위한 DTO 생성

---
+++ src/main/java/com/teamharmony/newscommunity/bookmarks/dto/RequestBookmarkDTO.java
@@ -0,0 +1,9 @@
+package com.teamharmony.newscommunity.bookmarks.dto;
+
+import lombok.Getter;
+
+@Getter
+public class RequestBookmarkDTO {
+    private String newsId;
+    private String userId;
+}
