58c4ea39f641e8d32887041928762baafda951c2
Yeah-PC\blueflow <blueflow109@gmail.com>
2022-07-22 17:29:10 +0900

    Fix: GET 요청 모두 허용하도록 변경 #156

--- src/main/java/com/teamharmony/newscommunity/auth/config/security/SecurityConfig.java
+++ src/main/java/com/teamharmony/newscommunity/auth/config/security/SecurityConfig.java
@@ -36,18 +36,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean(), userDetailsService, tokensRepository);
 		customAuthenticationFilter.setFilterProcessesUrl("/api/login");
-		http.csrf()
-		    .ignoringAntMatchers("/h2-console/**")
-		    .disable();
+		http.csrf().disable();
 		http.formLogin().disable();
 		http.httpBasic().disable();
 		http.sessionManagement().sessionCreationPolicy(STATELESS);
-		http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
 		
-		http.authorizeRequests().antMatchers("/api/login/**", "/api/token/**", "/api/signup/**", "/api/news/**", "/api/bookmarks/**", "/api/supports/**").permitAll();
-		http.authorizeRequests().antMatchers(GET, "/api/user/**").hasAuthority("ROLE_USER");
-		http.authorizeRequests().antMatchers(GET, "/api/admin/**").hasAuthority("ROLE_ADMIN");
-		http.authorizeRequests().anyRequest().authenticated();
+		http.authorizeRequests()
+		    .antMatchers("/api/login/**", "/api/signup", "/api/news/**", "/api/bookmarks/**").permitAll()
+		    .antMatchers(GET, "/api/**").permitAll()
+		    .antMatchers("/api/user/**").hasAuthority("ROLE_USER")
+				.antMatchers("/api/admin/**").hasAuthority("ROLE_ADMIN")
+				.anyRequest().authenticated();
 		
 		http.addFilter(customAuthenticationFilter);
 		// this filter comes before the other filters
