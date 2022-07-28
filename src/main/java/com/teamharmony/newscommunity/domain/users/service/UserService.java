40d226882fdf2e5da01659bef42e8234a6599a5d
Yeah-PC\blueflow <blueflow109@gmail.com>
2022-07-29 00:34:51 +0900

    Comment: 들여쓰기 포함 주석 수정 #230

--- src/main/java/com/teamharmony/newscommunity/domain/users/service/UserService.java
+++ src/main/java/com/teamharmony/newscommunity/domain/users/service/UserService.java
@@ -50,9 +50,9 @@ public class UserService implements UserDetailsService {
 	/**
 	 * 인증을 위한 사용자 조회
 	 *
-	 * @param 		username 해당 사용자 ID
-	 * @return 		사용자 정보를 담은 객체
-	 * @see   		UserDetailsService#loadUserByUsername
+	 * @param username 해당 사용자 ID
+	 * @return 사용자 정보를 담은 객체
+	 * @see UserDetailsService#loadUserByUsername
 	 */
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -69,8 +69,8 @@ public class UserService implements UserDetailsService {
 	/**
 	 * 사용자 ID 중복 여부
 	 *
-	 * @param 		username 중복 확인할 사용자 ID
-	 * @return 		사용자 ID 중복 여부
+	 * @param username 중복 확인할 사용자 ID
+	 * @return 사용자 ID 중복 여부
 	 */
 	public Map<String, Boolean> checkUser(String username) {
 		log.info("Checking duplicates username {}", username);
@@ -80,6 +80,12 @@ public class UserService implements UserDetailsService {
 		return body;
 	}
 
+	/**
+	 * 회원 가입
+	 *
+	 * @param dto 가입 시 입력한 정보
+	 * @return 성공 확인
+	 */
 	public String signUp(SignupRequestDto dto) throws HibernateException {
 		User user = new User(dto);
 		saveUser(user);
@@ -102,7 +108,7 @@ public class UserService implements UserDetailsService {
 	/**
 	 * 저장소에 사용자 저장
 	 *
-	 * @param 		user 저장할 사용자 정보
+	 * @param user 저장할 사용자 정보
 	 */
 	public void saveUser(User user) {
 		log.info("Saving new user {} to the database", user.getUsername());
@@ -113,7 +119,7 @@ public class UserService implements UserDetailsService {
 	/**
 	 * 저장소에 권한 저장
 	 *
-	 * @param 		role 저장할 권한 정보
+	 * @param role 저장할 권한 정보
 	 */
 	public void saveRole(Role role) {
 		log.info("Saving new role {} to the database", role.getName());
@@ -121,9 +127,9 @@ public class UserService implements UserDetailsService {
 	}
 	
 	/**
-	 * 회원 가입시 기본 프로필 적용
+	 * 회원 가입 시 기본 프로필 적용
 	 *
-	 * @param 		user 기본 프로필을 적용할 사용자 정보
+	 * @param user 기본 프로필을 적용할 사용자 정보
 	 */
 	public void defaultProfile(User user) {
 		UserProfile profile = UserProfile.builder()
@@ -137,8 +143,8 @@ public class UserService implements UserDetailsService {
 	/**
 	 * 사용자에 권한 부여
 	 *
-	 * @param 		username 권한을 추가할 사용자 ID
-	 * @param 		roleName 추가할 권한명
+	 * @param username 권한을 추가할 사용자 ID
+	 * @param roleName 추가할 권한명
 	 */
 	public void addRoleToUser(String username, RoleType roleName) throws IllegalArgumentException {
 		log.info("Adding role {} to user {}", roleName, username);
@@ -156,8 +162,8 @@ public class UserService implements UserDetailsService {
 	/**
 	 * 사용자 정보 조회
 	 *
-	 * @param 		username 조회할 사용자 ID
-	 * @return 		사용자 정보
+	 * @param username 조회할 사용자 ID
+	 * @return 사용자 정보
 	 */
 	private User getUser(String username) {
 		log.info("Fetching user {}", username);
@@ -167,8 +173,8 @@ public class UserService implements UserDetailsService {
 	/**
 	 * 권한 정보 조회
 	 *
-	 * @param 		roleName 조회할 권한명
-	 * @return 		권한 정보
+	 * @param roleName 조회할 권한명
+	 * @return 권한 정보
 	 */
 	private Role getRole(RoleType roleName) {
 		log.info("Fetching role {}", roleName);
@@ -178,20 +184,20 @@ public class UserService implements UserDetailsService {
 	/**
 	 * 전체 사용자 조회
 	 *
-	 * @return 		전체 사용자 정보
+	 * @return 전체 사용자 정보
 	 */
 	public List<UserResponseDto> getUsers() {
 		log.info("Fetching all users");
 		List<User> users= userRepository.findAll();
 		return users.stream().map(UserResponseDto::toDto).collect(toList());
 	}
-	
+
 	/**
 	 * 프로필 정보 조회
 	 *
-	 * @param 		username 프로필 정보를 가져올 회원 ID
-	 * @param 		status 인증된 사용자 ID와 일치 여부
-	 * @return 		인증된 사용자 ID와 일치 여부, 프로필 사진 url, 프로필 정보
+	 * @param username 프로필 정보를 가져올 회원 ID
+	 * @param status 인증된 사용자 ID와 일치 여부
+	 * @return 인증된 사용자 ID와 일치 여부, 프로필 사진 url, 프로필 정보
 	 */
 	public Map<String, Object> getProfile(String username, boolean status) {
 		log.info("Fetching profile of user {}", username);
@@ -208,8 +214,8 @@ public class UserService implements UserDetailsService {
 	/**
 	 * 버킷에 저장된 프로필 사진 조회
 	 *
-	 * @param 		username 프로필 정보를 가져올 회원 ID
-	 * @return 		프로필 사진 URL
+	 * @param username 프로필 정보를 가져올 회원 ID
+	 * @return 프로필 사진 URL
 	 */
 	public String getProfileImageUrl(String username) {
 		UserProfile profile = getUser(username).getProfile();
@@ -230,9 +236,9 @@ public class UserService implements UserDetailsService {
 	/**
 	 * 프로필 정보 변경
 	 *
-	 * @param 		username 프로필을 변경할 사용자 ID
-	 * @param 		requestDto 변경할 프로필 정보
-	 * @return 		성공 확인, 메시지
+	 * @param username 프로필을 변경할 사용자 ID
+	 * @param requestDto 변경할 프로필 정보
+	 * @return 성공 확인, 메시지
 	 */
 	public Map<String, String> updateProfile(String username, ProfileRequestDto requestDto) {
 		UserProfile existingProfile = getUser(username).getProfile();  // 해당 유저의 기존 프로필 찾기
@@ -243,7 +249,7 @@ public class UserService implements UserDetailsService {
 		                                                          .build();
 
 		MultipartFile file = requestDto.getFile();
-		if (file!=null) {
+		if (file != null) {
 			isImage(file); // 파일이 이미지인지 확인
 			// 버킷에 저장될 경로, 파일명 그리고 파일의 metadata 생성
 			String path = String.format("%s/%s", bucketName, username);
