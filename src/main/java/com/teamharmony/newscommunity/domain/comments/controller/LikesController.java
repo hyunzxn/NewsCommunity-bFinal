026333966207b603af540b5d4f7bd6bcf2203e16
hyunzxn <jann1653@gmail.com>
2022-07-28 15:03:29 +0900

    Feat: likesController swagger 적용 #221

--- src/main/java/com/teamharmony/newscommunity/domain/comments/controller/LikesController.java
+++ src/main/java/com/teamharmony/newscommunity/domain/comments/controller/LikesController.java
@@ -2,6 +2,10 @@ package com.teamharmony.newscommunity.domain.comments.controller;
 
 import com.teamharmony.newscommunity.domain.comments.service.LikesService;
 import com.teamharmony.newscommunity.common.annotation.CurrentUser;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +23,11 @@ public class LikesController {
      * @param id
      * @param username
      */
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "댓글의 아이디", required = true, dataType = "int", paramType = "path"),
+            @ApiImplicitParam(name = "username", value = "현재 로그인 한 유저", required = true, paramType = "body")
+    })
+    @ApiOperation(value = "좋아요 작동")
     @PostMapping("/user/likes/{id}")
     public ResponseEntity<?> likes(@PathVariable Long id,
                                    @CurrentUser String username) {
@@ -31,11 +40,20 @@ public class LikesController {
      * @param commentId
      * @return
      */
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "commentId", value = "댓글의 아이디", required = true, dataType = "int", paramType = "path")
+    })
+    @ApiOperation(value = "좋아요 개수 조회")
     @GetMapping("/user/likes/count/{commentId}")
     public ResponseEntity<Integer> getLikedCommentsCountByCommentId(@PathVariable Long commentId) {
         return ResponseEntity.ok().body(likesService.LikedCommentsCount(commentId));
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "commentId", value = "댓글의 아이디", required = true, dataType = "int", paramType = "path"),
+            @ApiImplicitParam(name = "username", value = "현재 로그인 한 유저", required = true, paramType = "body")
+    })
+    @ApiOperation(value = "좋아요 여부 확인")
     @GetMapping("/user/likes/isLiked/{commentId}")
     public ResponseEntity<Boolean> isLiked(@PathVariable Long commentId, @CurrentUser String username) {
         return ResponseEntity.ok().body(likesService.isLiked(commentId, username));
