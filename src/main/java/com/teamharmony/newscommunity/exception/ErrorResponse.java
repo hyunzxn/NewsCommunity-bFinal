b70cd40c18798a43abe78548ca2ed67e7d36f075
Yeah-PC\blueflow <blueflow109@gmail.com>
2022-07-28 20:15:58 +0900

    Fix: 전역 예외 처리에서 필요한 Error 클래스 필드 변경 #236

--- src/main/java/com/teamharmony/newscommunity/exception/ErrorResponse.java
+++ src/main/java/com/teamharmony/newscommunity/exception/ErrorResponse.java
@@ -1,5 +1,6 @@
 package com.teamharmony.newscommunity.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,18 +10,14 @@ import java.util.List;
 public class ErrorResponse {
     String statusCode;
     String requestUrl;
-    String code;
-    String message;
     String resultCode;
 
     List<Error> errorList;
 
     @Builder
-    public ErrorResponse(String statusCode, String requestUrl, String code, String message, String resultCode, List<Error> errorList){
+    public ErrorResponse(String statusCode, String requestUrl, String resultCode, List<Error> errorList){
         this.statusCode = statusCode;
         this.requestUrl = requestUrl;
-        this.code = code;
-        this.message = message;
         this.resultCode = resultCode;
         this.errorList = errorList;
     }
