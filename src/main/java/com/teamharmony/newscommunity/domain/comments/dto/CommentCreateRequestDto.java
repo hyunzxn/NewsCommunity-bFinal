c52fc79de929ca3cae111ba7ae6a6e0eb1deaa76
hyunzxn <jann1653@gmail.com>
2022-07-28 14:47:44 +0900

    Feat: Dto에 swagger 적용 #221

--- src/main/java/com/teamharmony/newscommunity/domain/comments/dto/CommentCreateRequestDto.java
+++ src/main/java/com/teamharmony/newscommunity/domain/comments/dto/CommentCreateRequestDto.java
@@ -1,5 +1,6 @@
 package com.teamharmony.newscommunity.domain.comments.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,8 +14,11 @@ import javax.validation.constraints.Size;
 @Setter
 @Getter
 public class CommentCreateRequestDto {
+
     @Size(max = 300, message = "글자수 초과")
+    @ApiModelProperty(value = "댓글 내용", example = "기사가 흥미롭네요", required = true)
     private String content;
 
+    @ApiModelProperty(value = "뉴스 아이디", example = "05c0a832-e8c3-4f16-8792-5c3778", required = true)
     private String newsId;
 }
--- src/main/java/com/teamharmony/newscommunity/domain/comments/dto/CommentEditRequestDto.java
+++ src/main/java/com/teamharmony/newscommunity/domain/comments/dto/CommentEditRequestDto.java
@@ -1,5 +1,6 @@
 package com.teamharmony.newscommunity.domain.comments.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,5 +14,6 @@ import javax.validation.constraints.Size;
 @Getter
 public class CommentEditRequestDto {
     @Size(max = 300, message = "글자수 초과")
+    @ApiModelProperty(value = "수정한 댓글 내용", example = "좋은 기사 감사합니다")
     private String content;
 }
