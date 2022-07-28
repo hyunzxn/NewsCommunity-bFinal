efd133f27e4bca257b415983eddb6a51fa11fdc8
Yeah-PC\blueflow <blueflow109@gmail.com>
2022-07-28 20:21:36 +0900

    Feat: 예외 처리 시 필드가 null일 경우 생략되도록 함 #236

--- src/main/java/com/teamharmony/newscommunity/exception/Error.java
+++ src/main/java/com/teamharmony/newscommunity/exception/Error.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 public class Error {
     private String field;
--- src/main/java/com/teamharmony/newscommunity/exception/ErrorResponse.java
+++ src/main/java/com/teamharmony/newscommunity/exception/ErrorResponse.java
@@ -6,6 +6,7 @@ import lombok.Data;
 
 import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 public class ErrorResponse {
     String statusCode;
