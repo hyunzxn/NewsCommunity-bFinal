f0ba933315cac8fa7a5ba5c18d25084adde460ab
hyeoKing <cksgurwkd12@naver.com>
2022-07-29 02:02:30 +0900

    Doc: News도메인에 Dto에 스웨거 적용 #240

--- src/main/java/com/teamharmony/newscommunity/domain/news/dto/CreateNewsAccessLogRequestDto.java
+++ src/main/java/com/teamharmony/newscommunity/domain/news/dto/CreateNewsAccessLogRequestDto.java
@@ -1,5 +1,6 @@
 package com.teamharmony.newscommunity.domain.news.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,8 +13,11 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class CreateNewsAccessLogRequestDto {
+    @ApiModelProperty(value = "뉴스 식별 id", example = "02c1e768-a5c0-4958-aa44-130512", required = true)
     private String news_id;
+    @ApiModelProperty(value = "유저 식별 id", example = "chlcksgur1", required = true)
     private String user_id;
+    @ApiModelProperty(value = "제목", example = "아데산야 충격적인 패배", required = true)
     private String title;
 
     /**
