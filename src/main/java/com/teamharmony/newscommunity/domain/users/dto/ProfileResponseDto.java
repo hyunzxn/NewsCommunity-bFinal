727c26036129e26500cc72e73a3c19b916ea8a75
hyunzxn <jann1653@gmail.com>
2022-07-30 11:31:43 +0900

    Enhance: Example 출력 위해 ProfileResponseDto에 @ApiModelProperty 추가 #268

--- src/main/java/com/teamharmony/newscommunity/domain/users/dto/ProfileResponseDto.java
+++ src/main/java/com/teamharmony/newscommunity/domain/users/dto/ProfileResponseDto.java
@@ -1,13 +1,18 @@
 package com.teamharmony.newscommunity.domain.users.dto;
 
 import com.teamharmony.newscommunity.domain.users.entity.UserProfile;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 
 @Getter
 public class ProfileResponseDto {
+	@ApiModelProperty(value = "유저 아이디", example = "guswns", required = true)
 	private String username;
+	@ApiModelProperty(value = "유저 닉네임", example = "김쫀떡", required = true)
 	private String nickname;
+	@ApiModelProperty(value = "유저 프로필 사진 링크", example = "KakaoTalk_20220611_160613387.jpg", required = true)
 	private String profile_pic;
+	@ApiModelProperty(value = "유저 프로필 소개", example = "반가워요!", required = true)
 	private String profile_info;
 	
 	public ProfileResponseDto(UserProfile profile) {
