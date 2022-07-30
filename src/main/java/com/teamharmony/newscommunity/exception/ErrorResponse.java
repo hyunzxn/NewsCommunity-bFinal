ef1c8fa114cf0bb48c24f015792039283724a03e
hyunzxn <jann1653@gmail.com>
2022-07-30 11:33:58 +0900

    Enhance: 에러 Example 위해서 @ApiModelProperty 추가 #268

--- src/main/java/com/teamharmony/newscommunity/exception/ErrorResponse.java
+++ src/main/java/com/teamharmony/newscommunity/exception/ErrorResponse.java
@@ -1,6 +1,7 @@
 package com.teamharmony.newscommunity.exception;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -16,8 +17,11 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 public class ErrorResponse {
+    @ApiModelProperty(value = "에러 상태 코드", example = "C402", required = true)
     String statusCode;
+    @ApiModelProperty(value = "요청 URL", example = "api/user/comments/{id}", required = true)
     String requestUrl;
+    @ApiModelProperty(value = "결과 코드", example = "FAIL", required = true)
     String resultCode;
 
     List<Error> errorList;
