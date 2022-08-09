47a2c91bdb6b8965ea7642011af4f871d42cadad
Yeah-PC\blueflow <blueflow109@gmail.com>
2022-08-09 23:02:53 +0900

    Feat: auth 패키지 스웨거 적용 #247

--- src/main/java/com/teamharmony/newscommunity/domain/auth/dto/SigninRequestDto.java
+++ src/main/java/com/teamharmony/newscommunity/domain/auth/dto/SigninRequestDto.java
@@ -1,14 +1,17 @@
 package com.teamharmony.newscommunity.domain.auth.dto;
 
 import com.teamharmony.newscommunity.domain.auth.util.SigninValidator;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 
 import javax.validation.constraints.Pattern;
 
 @Getter
 public class SigninRequestDto {
+	@ApiModelProperty(value = "로그인 ID", example = "chlcksgur1", required = true)
 	@Pattern(regexp = "^(?=.*[a-zA-Z])[-a-zA-Z0-9_.]{2,10}$")
 	private String username;
+	@ApiModelProperty(value = "로그인 PW", example = "secret123", required = true)
 	@Pattern(regexp = "^(?=.*\\d)(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,20}$")
 	private String password;
 	
