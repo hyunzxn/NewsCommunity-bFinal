056773d62ecd5fd41ccc542be4a0543e011d8fd7
Yeah-PC\blueflow <blueflow109@gmail.com>
2022-08-09 22:59:09 +0900

    Feat: users 패키지 스웨거 적용 #247

--- src/main/java/com/teamharmony/newscommunity/domain/users/controller/UserController.java
+++ src/main/java/com/teamharmony/newscommunity/domain/users/controller/UserController.java
@@ -1,15 +1,16 @@
 package com.teamharmony.newscommunity.domain.users.controller;
 
 import com.teamharmony.newscommunity.common.annotation.CurrentUser;
+import com.teamharmony.newscommunity.domain.users.dto.GetProfileResponseDto;
 import com.teamharmony.newscommunity.domain.users.dto.ProfileRequestDto;
 import com.teamharmony.newscommunity.domain.users.dto.UserResponseDto;
 import com.teamharmony.newscommunity.domain.users.entity.Role;
 import com.teamharmony.newscommunity.domain.users.entity.RoleType;
 import com.teamharmony.newscommunity.domain.users.service.UserService;
 import com.teamharmony.newscommunity.domain.users.dto.SignupRequestDto;
+import io.swagger.annotations.*;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.HibernateException;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -39,8 +40,12 @@ public class UserController {
 	 * @return 중복 여부
 	 * @see UserService#checkUser
 	 */
+	@ApiOperation(value = "ID 중복 확인", notes = "중복되는 ID가 있는지 확인")
+	@ApiImplicitParams({
+			@ApiImplicitParam(name = "requestDto", value = "가입하려는 사용자 ID", required = true, dataType = "SignupRequestDto.CheckDup")
+	})
 	@PostMapping("/signup/checkdup")
-	public ResponseEntity<Map<String, Boolean>> checkUser(@RequestBody @Valid SignupRequestDto.CheckDup requestDto) {
+	public ResponseEntity<Boolean> checkUser(@RequestBody @Valid SignupRequestDto.CheckDup requestDto) {
 		return ResponseEntity.ok().body(userService.checkUser(requestDto.username));
 	}
 
@@ -51,6 +56,10 @@ public class UserController {
 	 * @return 성공 확인
 	 * @see UserService#signUp
 	 */
+	@ApiOperation(value = "회원 가입", notes = "요청받은 ID와 PW로 회원 생성")
+	@ApiResponses({
+			@ApiResponse(code = 200, message = "OK", examples = @Example(value = @ExampleProperty(value = "success"))),
+	})
 	@PostMapping("/signup")
 	public ResponseEntity<String> signUp(@RequestBody @Valid SignupRequestDto requestDto) {
 		return ResponseEntity.ok().body(userService.signUp(requestDto));
@@ -63,6 +72,10 @@ public class UserController {
 	 * @return 인증된 사용자 ID
 	 * @see UserDetails#getUsername
 	 */
+	@ApiOperation(value = "인증된 사용자 ID 조회", notes = "헤더로 전달된 접근 토큰으로 사용자 ID 확인")
+	@ApiResponses({
+			@ApiResponse(code = 200, message = "OK", examples = @Example(value = @ExampleProperty(value = "chlcksgur1"))),
+	})
 	@GetMapping("/user/me")
 	public ResponseEntity<String> getUsername(@CurrentUser String username) {
 		return ResponseEntity.ok().body(username);
@@ -74,8 +87,9 @@ public class UserController {
 	 * @param 		role enum 타입 권한명이 담긴 객체
 	 * @see				UserService#saveRole
 	 */
+	@ApiOperation(value = "권한 추가", notes = "Enum으로 지정된 권한명을 입력하여 권한 추가")
 	@PostMapping("/admin/role/save")
-	public ResponseEntity<Void> saveUser(@RequestBody Role role) {
+	public ResponseEntity<Void> saveRole(@RequestBody Role role) {
 		userService.saveRole(role);
 		return ResponseEntity.ok().build();
 	}
@@ -86,6 +100,7 @@ public class UserController {
 	 * @param form 사용자 ID와 권한명이 담긴 객체
 	 * @see UserService#addRoleToUser
 	 */
+	@ApiOperation(value = "사용자에 권한 부여", notes = "요청받은 사용자 ID와 권한명으로 사용자에 특정 권한 부여")
 	@PostMapping("/admin/role/addtouser")
 	public ResponseEntity<Void> addRoleToUser(@RequestBody RoleToUserForm form) {
 		userService.addRoleToUser(form.getUsername(), form.getRoleName());
@@ -98,6 +113,7 @@ public class UserController {
 	 * @return 전체 유저를 담은 List
 	 * @see UserService#getUsers
 	 */
+	@ApiOperation(value = "전체 회원 조회", notes = "전체 회원을 조회")
 	@GetMapping("/admin/users")
 	public ResponseEntity<List<UserResponseDto>> getUsers() {
 		return ResponseEntity.ok().body(userService.getUsers());
@@ -111,12 +127,16 @@ public class UserController {
 	 * @return 인증된 사용자 ID와 일치 여부, 프로필 사진 url, 프로필 정보
 	 * @see UserService#getProfile
 	 */
+	@ApiOperation(value = "프로필 정보 조회", notes = "요청받은 프로필 회원 ID의 프로필 정보를 조회하고 현재 사용자와 일치하는지 확인")
+	@ApiImplicitParams({
+			@ApiImplicitParam(name = "profileOwner", value = "프로필 회원 ID", required = true, dataType = "String", example = "chlcksgur1")
+	})
 	@GetMapping("/user/profile/{profileOwner}")
-	public ResponseEntity<Map<String, Object>> getProfile(@PathVariable String profileOwner, @CurrentUser String username) {
+	public ResponseEntity<GetProfileResponseDto> getProfile(@PathVariable String profileOwner, @CurrentUser String username) {
 		boolean status = profileOwner.equals(username);
 		return ResponseEntity.ok().body(userService.getProfile(profileOwner, status));
 	}
-	
+
 	/**
 	 * 회원 프로필 사진 URL 조회
 	 *
@@ -124,6 +144,11 @@ public class UserController {
 	 * @return 프로필 사진 url
 	 * @see UserService#getProfileImageUrl
 	 */
+	@ApiOperation(value = "프로필 사진 URL 조회", notes = "요청받은 프로필 회원 ID의 프로필 사진 URL을 조회(default일 경우 기본 이미지 적용)")
+	@ApiResponses({
+			@ApiResponse(code = 200, message = "OK", examples = @Example(value = @ExampleProperty(value = "https://s3.ap-northeast-2.amazonaws.com/harmony-profile/chlcksgur1/image.jpg"))),
+	})
+	@ApiImplicitParam(name = "username", value = "프로필 회원 ID", required = true, dataType = "String", example = "chlcksgur1")
 	@GetMapping("/user/profile/pic/{username}")
 	public ResponseEntity<String> getProfilePicUrl(@PathVariable String username) {
 		return ResponseEntity.ok().body(userService.getProfileImageUrl(username));
@@ -134,21 +159,26 @@ public class UserController {
 	 *
 	 * @param requestDto 닉네임, 프로필 사진 파일, 소개글을 담은 객체
 	 * @param username 인증된 사용자 정보
-	 * @return 성공 확인, 메시지
+	 * @return 성공 확인
 	 * @see UserService#updateProfile
 	 */
+	@ApiOperation(value = "프로필 변경", notes = "요청받은 프로필 정보로 프로필 변경")
+	@ApiResponses({
+			@ApiResponse(code = 200, message = "OK", examples = @Example(value = @ExampleProperty(value = "success"))),
+	})
 	@PostMapping(
 			path = "/user/update_profile",
-			consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
-			produces = MediaType.APPLICATION_JSON_VALUE
+			consumes = MediaType.MULTIPART_FORM_DATA_VALUE
 	)
-	public ResponseEntity<Map<String, String>> updateProfile(ProfileRequestDto requestDto, @CurrentUser String username) {
+	public ResponseEntity<String> updateProfile(ProfileRequestDto requestDto, @CurrentUser String username) {
 		return ResponseEntity.ok().body(userService.updateProfile(username, requestDto));
 	}
 
 	@Data
 	class RoleToUserForm{
+		@ApiModelProperty(value = "사용자 ID", example = "chlcksgur1", required = true)
 		private String username;
+		@ApiModelProperty(value = "권한명", example = "USER", required = true)
 		private RoleType roleName;
 	}
 }
