87ddd03c6e1e7c5ad2784d4a36b576ceea789a98
Yeah-PC\blueflow <blueflow109@gmail.com>
2022-08-09 22:59:52 +0900

    Feat: supports 패키지 스웨거 적용 #247

--- src/main/java/com/teamharmony/newscommunity/domain/supports/dto/SupportRequestDto.java
+++ src/main/java/com/teamharmony/newscommunity/domain/supports/dto/SupportRequestDto.java
@@ -1,13 +1,17 @@
 package com.teamharmony.newscommunity.domain.supports.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 
 import javax.validation.constraints.Email;
 
 @Getter
 public class SupportRequestDto {
-    private String post_title;
-    private String post_content;
+	@ApiModelProperty(value = "게시물 제목", example = "문의드립니다.", required = true)
+	private String post_title;
+	@ApiModelProperty(value = "게시물 내용", example = "뉴스 업데이트가 언제 되나요?", required = true)
+	private String post_content;
+	@ApiModelProperty(value = "이메일", example = "teamharmoney22@gmail.com", required = true)
     @Email
     private String post_email;
 }
